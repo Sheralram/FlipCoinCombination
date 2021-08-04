1dfaf0562b07e3bb0dffc820dbb16607c658392b
Sheralram <ramkrishna.sheral@gmail.com>
2021-08-05 00:37:11 +0530

    Storing in Singlet Combination in Dictionary and Percentage

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -11,3 +11,26 @@ then
 else
         echo "TAILS"
 fi
+
+declare -A Singlet
+
+for ((i=1; i<=50; i++))
+do
+	randomCheck=$((RANDOM % 2))
+	if [ $randomCheck == 0 ]
+	then
+        	echo "HEADS"
+		((Singlet[H]++))
+	else
+		echo "TAILS"
+		((Singlet[T]++))
+	fi
+done
+
+echo ${!Singlet[@]}
+echo ${Singlet[@]}
+
+HeadPC=`awk "BEGIN {print (${Singlet[H]}/50)*100}"`
+echo "HEAD = $HeadPC%"
+TailPC=`awk "BEGIN {print (${Singlet[T]}/50)*100}"`
+echo "TAIL = $TailPC%"
